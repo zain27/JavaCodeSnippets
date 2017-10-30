a7e7f269b952d9fb5e82e023df921774e572821c
zain27 <zain27@users.noreply.github.com>
2017-10-30 17:30:45 -0400

    Update EmailValidation.java
    Added description and updated a few comments.

--- EmailValidation.java
+++ EmailValidation.java
@@ -1,26 +1,33 @@
+/*
+Rules: 
+Email addresses must contain only one (@)  and one (.), which must come after the @
+At least one character in the email address must be present before the (@), between the @ and the (.), and after the period
+
+ */
+
 class EmailValidation
 {
    public static void main(String args[])
    {
       String email ="something@somewhere.com"; // change to test out all scenarios
       boolean result = false;      
-      int index1 =0; // for @
-      int index2 =0; // for .
+      int index1 =0; // for index of @
+      int index2 =0; // for index of .
       int difference=0;
       
       result =  occurence(email,'@');      
       
-      if (result)  // if contains @
+      if (result)  // if contains @ then proceed to validate further
       {
          result =  occurence(email,'.');      
          if(result) // if contans .
          {
             index1=email.indexOf('@'); 
             index2=email.indexOf('.');  
-            if(index1 < index2) // if . after the @
+            if(index1 < index2) // if . after the @ then proceed to validate further
             {
                difference =index2 -index1;
-               if(index1==0) // if starts with @
+               if(index1==0) // if starts with @ 
                {
                   System.out.println("Email cannot start with @");                  
                }
@@ -61,18 +68,18 @@ class EmailValidation
       int ctr=0;
       int loopCounter=0;
       int index=0;
-      for (int i=0;i<email.length();i++)        
+      for (int i=0;i<email.length();i++)   // loop through the entire string and count the number of times the characters match.     
       {
          if(email.charAt(i) == ch)
          {
             ctr++;
          }
       }
-      if (ctr == 1)
+      if (ctr == 1) // if one match return true
       {
          return true;
       }
-      else 
+      else // if anything but 1, return false.
       {
          return false;
       }
